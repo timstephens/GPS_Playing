4500c347e0e2cb6bb9a3ce8ec36d318ddcec9839
Tim Stephens <tim@tjstephens.com>
2018-02-26 22:30:13 +0000

    Working with Radio
    Now able to send data over the RFM69

--- GPS-Playing/GPS-Playing.ino
+++ GPS-Playing/GPS-Playing.ino
@@ -1,7 +1,7 @@
 /* Connect to GPS receiver and broadcast the position fix down the serial port.
 
 
-// On a Pro mini, also need to remember to set the clock speed or your timings and baud rates will be out by a factor of 2 (8 vs 16MHz)
+  // On a Pro mini, also need to remember to set the clock speed or your timings and baud rates will be out by a factor of 2 (8 vs 16MHz)
     Radio communicates over SPI (10 -SS ,11 -MISO ,12 -MOSI,13 - SCK)
 
     Use the Serial port to connect to the GPS
@@ -9,38 +9,71 @@
 */
 
 #include <SoftwareSerial.h>
+// Include the RFM69 and SPI libraries:
+#include <RFM69.h>
+#include <SPI.h>
+
+#define NETWORKID 0 //Metwork ID
+#define MYNODEID  1  //Me
+#define TONODEID  2  //Where we are sending to...
+
+#define FREQUENCY     RF69_868MHZ
 
 
-SoftwareSerial debugSerial(8,9);
+#define ENCRYPT       false // Set to "true" to use encryption
+#define ENCRYPTKEY    "TOPSECRETPASSWRD" // Use the same 16-byte key on all nodes
 
+
+SoftwareSerial GPSSerial(8, 9);
+RFM69 radio;
 
 String myPosition;
+
+
 void setup() {
   //Setup Serial ports
 
   Serial.begin(9600);
-  debugSerial.begin(9600);
+  GPSSerial.begin(9600);
 
   //Setup radio (later)
   Serial.println("Setup");
 
+  // Initialize the RFM69HCW:
+
+  radio.initialize(FREQUENCY, MYNODEID, NETWORKID);
+  radio.setHighPower(); // Always use this for RFM69HCW
+
+  // Turn on encryption if desired:
+
+  if (ENCRYPT)
+    radio.encrypt(ENCRYPTKEY);
 }
+
+
+
 
 void loop() {
 
-  if(debugSerial.available()) {
+  if (GPSSerial.available()) {
     //Clear stuff from the buffer until there's something to read (look for $).
-//    while(!debugSerial.find('$')){
-//      //Trap the code here
-//    }
-      myPosition = debugSerial.readStringUntil('\n');
-      if(myPosition.startsWith("$GPGGA")) {
-            Serial.write(myPosition.c_str(), myPosition.length());
-      }
+    //    while(!GPSSerial.find('$')){
+    //      //Trap the code here
+    //    }
+    myPosition = GPSSerial.readStringUntil('\n');
+    if (myPosition.startsWith("$GPGGA")) {
+      //truncate to 62bytes (max length of a datablock)
+      myPosition = myPosition.substring(0,60);
+      Serial.write(myPosition.c_str(), myPosition.length());
+
+      //send stuff via the radio link
+      radio.send(TONODEID, myPosition.c_str(), myPosition.length());
+
+    }
 
 
-    //Need to decode NEMA sentences now. 
-    
+    //Need to decode NEMA sentences now.
+
   }
   delay(100);
   Serial.println(".");
