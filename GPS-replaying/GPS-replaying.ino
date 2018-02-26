d3e1145f80cea78572130b8e77183bf2f1631e67
Tim Stephens <tim@tjstephens.com>
2018-02-26 22:31:08 +0000

    Add receiver end
    This sketch sits on the receiving end and spits the data it gets out of
    the serial line for debug and onward transmission

---
+++ GPS-replaying/GPS-replaying.ino
@@ -0,0 +1,68 @@
+#include <RFM69.h>
+#include <SPI.h>
+
+// Addresses for this node. CHANGE THESE FOR EACH NODE!
+
+#define NETWORKID     0   // Must be the same for all nodes
+#define MYNODEID      2   // My node ID
+#define TONODEID      1   // Destination node ID
+
+// RFM69 frequency, uncomment the frequency of your module:
+#define FREQUENCY     RF69_868MHZ
+
+// AES encryption (or not):
+
+#define ENCRYPT       false // Set to "true" to use encryption
+#define ENCRYPTKEY    "TOPSECRETPASSWRD" // Use the same 16-byte key on all nodes
+
+RFM69 radio;
+
+void setup()
+{
+  // Open a serial port so we can send keystrokes to the module:
+
+  Serial.begin(57600);
+  Serial.print("Node ");
+  Serial.print(MYNODEID, DEC);
+  Serial.println(" ready");
+
+
+  // Initialize the RFM69HCW:
+
+  radio.initialize(FREQUENCY, MYNODEID, NETWORKID);
+  radio.setHighPower(); // Always use this for RFM69HCW
+
+  // Turn on encryption if desired:
+
+}
+
+void loop()
+{
+
+  // RECEIVING
+
+  // In this section, we'll check with the RFM69HCW to see
+  // if it has received any packets:
+
+  if (radio.receiveDone()) // Got one!
+  {
+    // Print out the information:
+
+    Serial.print("received from node ");
+    Serial.print(radio.SENDERID, DEC);
+    Serial.print(", message [");
+
+    // The actual message is contained in the DATA array,
+    // and is DATALEN bytes in size:
+
+    for (byte i = 0; i < radio.DATALEN; i++)
+      Serial.print((char)radio.DATA[i]);
+
+    // RSSI is the "Receive Signal Strength Indicator",
+    // smaller numbers mean higher power.
+
+    Serial.print("], RSSI ");
+    Serial.println(radio.RSSI);
+
+  }
+}
